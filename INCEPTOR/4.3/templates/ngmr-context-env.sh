df6e3f865cfa91485594e9e0e98abc6c810aa78c
xiaolun.li <xiaolun.li@transwarp.io>
2016-11-23 08:28:04 +0000

    WARP-9459: add INCEPTOR templates based on Deployment

---
+++ INCEPTOR/4.3/templates/ngmr-context-env.sh
@@ -0,0 +1,24 @@
+<#--handle dependent.zookeeper-->
+<#if dependencies.ZOOKEEPER??>
+  <#assign zookeeper=dependencies.ZOOKEEPER quorums=[]>
+  <#list zookeeper.roles.ZOOKEEPER as role>
+    <#assign quorums += [role.hostname + ":" + zookeeper[role.hostname]["zookeeper.client.port"]]>
+  </#list>
+  <#assign quorum = quorums?join(",")>
+</#if>
+
+export NGMR_YARN=true
+
+export INCEPTOR_YARN_MASTER_MEMORY=512M
+export INCEPTOR_YARN_EXECUTOR_MEMORY=1569M
+export INCEPTOR_YARN_EXECUTOR_CORES=3
+export NGMR_YARN_EXECUTOR_LIST=
+export INCEPTOR_YARN_EXECUTOR_MEM_CPU_RATIO=
+export INCEPTOR_YARN_EXECUTOR_USED_RESOURCE_PERCENT=
+export INCEPTOR_YARN_NUMBER_EXECUTORS=-1
+export INCEPTOR_YARN_NUMBER_WORKERS=-1
+export INCEPTOR_YARN_SCHEDULER_MODE=GreedyMode
+export SPARK_YARN_APP_NAME=${service.sid}-${localhostname}-inceptorserver
+export SPARK_YARN_QUEUE=default
+export INCEPTOR_LICENSE_ZOOKEEPER_QUORUM=${quorum}
+export INCEPTOR_UI_PORT=4040
---
+++ INCEPTOR/4.3/templates/ngmr-env.sh
@@ -0,0 +1,7 @@
+#NGMR CONF ENVIRONMENT
+export NGMR_LOCALDIR="/hadoop/ngmr/${service.sid}"
+export NGMR_FASTDISK_DIR="/mnt/ramdisk/ngmr"
+export NGMR_FASTDISK_SIZE=0.5
+export NGMR_CACHE_SIZE=0.5
+export NGMR_EXECUTORS_PERJOB=1
+export INCEPTOR_LOG_DIR=/var/log/${service.sid}
