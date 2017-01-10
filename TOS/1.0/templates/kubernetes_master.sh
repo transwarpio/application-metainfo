4ef4006983cb9be39ade052d644bd0857a9e2033
xinhui <xinhui.li@transwarp.io>
2017-01-10 06:17:43 +0000

    WARP-7684: registry.port as service level config

--- TOS/1.0/templates/kubernetes_master.sh
+++ TOS/1.0/templates/kubernetes_master.sh
@@ -1,2 +1,2 @@
 <#assign tosMaster=service.roles['TOS_MASTER'][0].hostname>
-export KUBERNETES_MASTER="http://${tosMaster}:${service[tosMaster]['tos.master.apiserver.port']}"
+export KUBERNETES_MASTER="http://${tosMaster}:${service['tos.master.apiserver.port']}"
