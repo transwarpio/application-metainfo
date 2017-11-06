561f5296084ed58416e5b768a537e7594156a2e1
xinhui <xinhui.li@transwarp.io>
2017-11-06 17:09:53 +0800

    metainfo CI

---
+++ build_script/image/build_application-metainfo.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+build_application-metainfo() {
+    SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+    META_SRC_DIR="$SCRIPT_DIR"/../..
+    TARGET_DIR="$META_SRC_DIR/target"
+
+    mkdir -p "$TARGET_DIR"
+
+    for VERSION in transwarp-5.1 transwarp-5.0 transwarp-ce-1.0; do
+        echo "preparing $VERSION ..."
+
+        META_DST_DIR="$TARGET_DIR/$VERSION"
+
+        cd "$META_SRC_DIR"
+        dirs=()
+        while IFS=  read -r -d $'\0'; do
+          dirs+=("$REPLY")
+        done < <(find . -name "$VERSION" -print0)
+        if [ -d "$META_DST_DIR" ]; then
+          rm -rf "$META_DST_DIR"
+        fi
+        mkdir -p "$META_DST_DIR"
+        for dir in "${dirs[@]}"; do
+          mkdir -p "$META_DST_DIR/$dir"
+          cp -rp $dir/* "$META_DST_DIR/$dir/"
+        done
+
+
+        echo "building $VERSION image ..."
+        cd "$TARGET_DIR"
+        rm -rf application-metainfo
+        mv "$VERSION" application-metainfo
+        echo 'FROM 172.16.1.41:5000/release/basefs:latest
+
+RUN mkdir -p /root/application-metainfo
+ADD application-metainfo /root/application-metainfo
+' > Dockerfile
+
+        POST_COMMIT_TAG="${DOCKER_REPO_URL}/postcommit/application-metainfo:$VERSION"
+        GOLD_TAG="${DOCKER_REPO_URL}/gold/application-metainfo:$VERSION"
+
+        docker build -t "$POST_COMMIT_TAG" .
+        docker push "$POST_COMMIT_TAG"
+        docker tag "$POST_COMMIT_TAG" "$GOLD_TAG"
+        docker push "$GOLD_TAG"
+    done
+}
