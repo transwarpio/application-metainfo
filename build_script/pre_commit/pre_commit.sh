5ca1d3d6da41f43961f213a730ecbe3a9c5fe428
xinhui <xinhui.li@transwarp.io>
2017-11-07 20:14:11 +0800

    pre commit

--- build_script/pre_commit/pre_commit.sh
+++ build_script/pre_commit/pre_commit.sh
@@ -1,13 +1,59 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-source "$SCRIPT_DIR/util/jenkins_utils.sh"
-source "$SCRIPT_DIR/util/git_utils.sh"
-
 set -e
+
+SUPPORTED_VERSIONS=(transwarp-5.1 transwarp-5.0 transwarp-ce-1.0)
+
+print_merge_params() {
+    echo "=================================================="
+    echo "gitlabActionType : ${gitlabActionType}"
+    echo "gitlabBranch : ${gitlabBranch}"
+    echo "gitlabMergeRequestId : ${gitlabMergeRequestId}"
+    echo "gitlabMergeRequestIid : ${gitlabMergeRequestIid}"
+    echo "gitlabMergeRequestLastCommit : ${gitlabMergeRequestLastCommit}"
+    echo "gitlabMergeRequestTitle : ${gitlabMergeRequestTitle}"
+    echo "gitlabSourceBranch : ${gitlabSourceBranch}"
+    echo "gitlabSourceNamespace : ${gitlabSourceNamespace}"
+    echo "gitlabSourceRepoHomepage : ${gitlabSourceRepoHomepage}"
+    echo "gitlabSourceRepoHttpUrl : ${gitlabSourceRepoHttpUrl}"
+    echo "gitlabSourceRepoName : ${gitlabSourceRepoName}"
+    echo "gitlabSourceRepoSshUrl : ${gitlabSourceRepoSshUrl}"
+    echo "gitlabSourceRepoURL : ${gitlabSourceRepoURL}"
+    echo "gitlabTargetBranch : ${gitlabTargetBranch}"
+    echo "gitlabTargetNamespace : ${gitlabTargetNamespace}"
+    echo "gitlabTargetRepoHttpUrl : ${gitlabTargetRepoHttpUrl}"
+    echo "gitlabTargetRepoName : ${gitlabTargetRepoName}"
+    echo "gitlabTargetRepoSshUrl : ${gitlabTargetRepoSshUrl}"
+    echo "gitlabUserEmail : ${gitlabUserEmail}"
+    echo "gitlabUserName : ${gitlabUserName}"
+    echo "=========================================================="
+}
+
+
+pull_merge_request() {
+    local temp_branch=${gitlabSourceNamespace}/${gitlabSourceRepoName}-${gitlabSourceBranch}
+
+    git fetch ${gitlabSourceRepoHttpUrl} ${gitlabSourceBranch}
+    git checkout -b ${temp_branch} FETCH_HEAD
+
+    git checkout ${gitlabTargetBranch}
+    git merge --no-ff ${temp_branch} --no-edit
+}
+
+# exported: AFFECTED_VERSIONS
+calc_affected_versions() {
+    AFFECTED_VERSIONS=()
+    for version in "${SUPPORTED_VERSIONS[@]}"; do
+        if git diff origin/${gitlabTargetBranch} | grep ${version} ; then
+            AFFECTED_VERSIONS+=(${version})
+        fi
+    done
+}
+
 
 print_merge_params
 
 pull_merge_request
 
-
+calc_affected_versions
+echo "affected versions: ${AFFECTED_VERSIONS[@]}"
