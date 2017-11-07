b01e057793c79b1163ae8d0cad0522b4aa6508c2
xinhui <xinhui.li@transwarp.io>
2017-11-07 18:53:16 +0800

    pre commit

--- build_script/pre_commit/pre_commit.sh
+++ build_script/pre_commit/pre_commit.sh
@@ -2,10 +2,12 @@
 
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source "$SCRIPT_DIR/util/jenkins_utils.sh"
+source "$SCRIPT_DIR/util/git_utils.sh"
 
 set -e
 
 print_merge_params
 
+pull_merge_request
 
 
