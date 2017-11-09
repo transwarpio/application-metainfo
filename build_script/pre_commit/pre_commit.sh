ef002f5e997dda6e87600a88dccba540b0c333bc
xinhui <xinhui.li@transwarp.io>
2017-11-09 17:46:40 +0800

    pre commit

--- build_script/pre_commit/pre_commit.sh
+++ build_script/pre_commit/pre_commit.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 SUPPORTED_VERSIONS=(transwarp-5.1 transwarp-5.0 transwarp-ce-1.0)
+VIRTUAL_MACHINES=(172.16.1.1:172.16.1.249 172.16.1.2:172.16.1.250 172.16.1.4:172.16.1.251)
+MANAGER_IP=172.16.1.251
+MANAGER_PORT=8180
 
 
 set -e
@@ -61,6 +64,32 @@ calc_affected_services() {
 }
 
 
+revert_virtual_machines() {
+    current_version=$1
+
+    for host_guest in "${VIRTUAL_MACHINES[@]}"; do
+        host_ip=$(echo ${host_guest} | cut -d':' -f1)
+        guest_ip=$(echo ${host_guest} | cut -d':' -f2)
+        guest_id="cloudservice_${guest_ip}_centos72"
+
+        ssh ${host_ip} virsh destroy ${guest_id}
+        ssh ${host_ip} virsh snapshot-revert ${guest_id} ${current_version}
+        ssh ${host_ip} virsh start ${guest_id}
+
+        for i in {1..60}; do
+            if ping -c 1 -w 2 ${guest_ip}; then
+                break
+            fi
+            sleep 1
+        done
+        if ! ping -c 1 -w 2 ${guest_ip}; then
+            exit 1
+        fi
+    done
+}
+
+
+
 
 
 print_merge_params
@@ -76,4 +105,6 @@ for version in "${AFFECTED_VERSIONS[@]}"; do
     echo "modified services: ${affected_services}"
     start_sequence=$(python "${SCRIPT_DIR}/util/get_sorted_releted_services.py" "${WORKSPACE}" ${version} ${affected_services})
     echo "start sequence: ${start_sequence}"
+
+    revert_virtual_machines ${version}
 done
