40eb4823eaa3f0287803413abe6c15a1c5bddfd0
jianli <jian.li@transwarp.io>
2018-04-01 10:53:27 +0800

    change jupyter conf

--- SOPHON/sophonweb-1.0.1-rc2/templates/jupyter_notebook_config.py
+++ SOPHON/sophonweb-1.0.1-rc2/templates/jupyter_notebook_config.py
@@ -40,7 +40,7 @@ c.NotebookApp.token = ''
 #  uses.
 c.NotebookApp.tornado_settings = {
     'headers': {
-        'Content-Security-Policy': "frame-ancestors http://${service.roles.SOPHON_WEB[0].hostname}:${service['sophon.server.port']} 'self' "
+        'Content-Security-Policy': "frame-ancestors http://*:${service['sophon.server.port']} 'self' "
     }
 }
 
