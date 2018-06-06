4ca9a0c922c64e3416599163f6fd3567ae0eb4a5
xinhui <leolee192@163.com>
2018-06-06 20:33:33 +0800

    hide non-final versions

--- MANAGER/transwarp-5.2.0-rc0/templates/zookeeper-zookeeper-env.sh
+++
@@ -1,6 +0,0 @@
-export ZOOKEEPER_CONF_DIR=/etc/${service.sid}/conf
-export ZOOKEEPER_LOG_DIR=/var/log/${service.sid}/zookeeper
-export ZOOKEEPER_DATA_DIR=/var/${service.sid}/zookeeper
-export SERVER_JVMFLAGS="-Dcom.sun.management.jmxremote.port=${service['zookeeper.jmxremote.port']} -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false $SERVER_JVMFLAGS"
-export SERVER_JVMFLAGS="-Xmx${service['zookeeper.server.memory']?number?floor}m $SERVER_JVMFLAGS"
-export SERVER_JVMFLAGS="-Dzookeeper.log.dir=/var/log/${service.sid}/zookeeper -Dzookeeper.root.logger=INFO,ROLLINGFILE $SERVER_JVMFLAGS"
