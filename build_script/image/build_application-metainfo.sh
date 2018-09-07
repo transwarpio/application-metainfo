64f7e796c1300d7b2e2cee3d587e1c8ca7dd3f60
xinhui <leolee192@163.com>
2018-09-07 10:58:14 +0800

    branch be master

--- build_script/image/build_application-metainfo.sh
+++ build_script/image/build_application-metainfo.sh
@@ -49,8 +49,8 @@ build_application-metainfo() {
         git remote rm origin
         git remote add origin https://github.com/transwarpio/application-metainfo.git
         http_proxy=http://172.16.2.14:7777 https_proxy=http://172.16.2.14:7777 git fetch origin
-        git checkout dev
-        git branch --set-upstream-to=origin/dev dev
+        git checkout master
+        git branch --set-upstream-to=origin/master master
 
 
         echo "building $VERSION image ..."
