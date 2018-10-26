ee44fa9a686d4f7c7cbb18cc07dd93341f18e23d
jianli <jian.li@transwarp.io>
2018-10-26 14:51:42 +0800

    add 2.0.0-rc2 and fix parameters

---
+++ build_script/util/tag_sophon.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+IN_PROGRESS_VERSION=sophon-2.0
+TAG_VERSION=sophon-2.0.0-rc2
+
+set -e
+
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+WORKSPACE="${SCRIPT_DIR}/../.."
+
+SERVICE_LIST=(SOPHON)
+
+for service in "${SERVICE_LIST[@]}"; do
+    if [ -d "${WORKSPACE}"/"${service}"/"${IN_PROGRESS_VERSION}" ]; then
+       rm -rf "${WORKSPACE}"/"${service}"/"${TAG_VERSION}"
+       cp -rp "${WORKSPACE}"/"${service}"/"${IN_PROGRESS_VERSION}" "${WORKSPACE}"/"${service}"/"${TAG_VERSION}"
+       for file in $(find "${WORKSPACE}"/"${service}"/"${TAG_VERSION}" | grep -v upgrade | grep -v metainfo.yaml); do
+           if [ -f ${file} ]; then
+              sed -i "s/${IN_PROGRESS_VERSION}/${TAG_VERSION}/g" "${file}"
+           fi
+       done
+       sed -i "s/version: ${IN_PROGRESS_VERSION}/version: ${TAG_VERSION}/" "${WORKSPACE}"/"${service}"/"${TAG_VERSION}"/metainfo.yaml
+       sed -i "/dockerImage/s#${IN_PROGRESS_VERSION}#${TAG_VERSION}#" "${WORKSPACE}"/"${service}"/"${TAG_VERSION}"/metainfo.yaml
+    fi
+done
+
