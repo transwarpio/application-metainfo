c1ebf2165693b9793f5fe0f7ef56752be77af95b
jianli <jian.li@transwarp.io>
2019-01-09 14:43:45 +0800

    add new roles for 2.2

--- SOPHON/sophon-2.2/templates/env.sh
+++ SOPHON/sophon-2.2/templates/env.sh
@@ -30,6 +30,7 @@ SPARK_HOME=/usr/lib/spark2
 SPARK_USER=hive
 HADOOP_USER_NAME=hive
 MIDAS_SERVER_JAVA_OPTS="-Xms512m -Xmx1024m -XX:PermSize=128m"
+SPARK_DIST_CLASSPATH=/usr/lib/sophon/hadoop-jars/*
 
 <#if service['sophon.resource.type'] = "yarn">
 HADOOP_CONF_DIR=/etc/${dependencies.YARN.sid}/conf
