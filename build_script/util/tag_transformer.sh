3d8298df2c23a0902d3c379c54ddce979ba922d0
xinhui.li <xinhui.li@transwarp.io>
2019-01-21 16:17:16 +0800

    manager-6.0.1812a-final

--- build_script/util/tag_transformer.sh
+++ build_script/util/tag_transformer.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 MANAGER_PROGRESS_VERSION=manager-6.0
-MANAGER_TAG_VERSION=manager-6.0.1812a-rc2
+MANAGER_TAG_VERSION=manager-6.0.1812a-final
 LAST_STABLE_VERSION=transwarp-6.0.0-final
 TDH_PROGRESS_VERSION=transwarp-6.0
-TDH_TAG_VERSION=transwarp-6.0.1-rc4
+TDH_TAG_VERSION=transwarp-6.0.1-final
 
 set -e
 
