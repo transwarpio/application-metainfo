9eeaa84f075157e933f64a0ee88e988e5867671b
JianLi <jian.li@transwarp.io>
2019-02-15 15:23:20 +0800

    edit 3.0 conf for sophon-base

--- SOPHON/sophon-3.0/templates/env.sh
+++ SOPHON/sophon-3.0/templates/env.sh
@@ -31,17 +31,18 @@ SPARK_USER=hive
 HADOOP_USER_NAME=hive
 MIDAS_SERVER_JAVA_OPTS="-Xms512m -Xmx1024m -XX:PermSize=128m"
 
-<#if dependencies.YARN??>
 HADOOP_CONF_DIR=/etc/${dependencies.YARN.sid}/conf
+HDFS_CONF_DIR=/etc/${dependencies.HDFS.sid}/conf  
+<#if service['sophon.resource.type'] != "yarn">
+SPARK_DIST_CLASSPATH=/usr/lib/sophon/hadoop-jars/*
 </#if>
+
 
 <#if dependencies.HYPERBASE??>
 HYPERBASE_CONF_DIR=/etc/${dependencies.HYPERBASE.sid}/conf
 </#if>
 
 LIVY_LOG_DIR=/var/log/midas
-DIEU_HOME=/usr/lib/sophon
-DIEU_CONF_DIR=/etc/${service.sid}/conf
 PYSPARK_PYTHON=python3
 PYSPARK_DRIVER_PYTHON=python3
 # for Jupyter notebook running on server
