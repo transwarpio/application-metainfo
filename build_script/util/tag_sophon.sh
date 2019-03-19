f6b97208a2276781ca35458486d992a7e582555c
JianLi <jian.li@transwarp.io>
2019-03-19 19:14:57 +0800

    add 2.3 conf

--- build_script/util/tag_sophon.sh
+++ build_script/util/tag_sophon.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-IN_PROGRESS_VERSION=sophon-2.2
-TAG_VERSION=sophon-2.2.0-final
+IN_PROGRESS_VERSION=sophon-3.0
+TAG_VERSION=sophon-2.3
 set -e
 
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
