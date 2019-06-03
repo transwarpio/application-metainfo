1d3a014886e8309bcf8b4e09745f5b8efab0143f
JianLi <jian.li@transwarp.io>
2019-06-03 16:13:50 +0800

    add sophon-2.3.0-rc2

--- build_script/util/tag_sophon.sh
+++ build_script/util/tag_sophon.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 IN_PROGRESS_VERSION=sophon-2.3
-TAG_VERSION=sophon-2.3.0-rc1
+TAG_VERSION=sophon-2.3.0-rc2
 set -e
 
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
