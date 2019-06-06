da27c0bcbb7804691731189c0a50c82ea418ce49
Ye Wu <ye.wu@transwarp.io>
2019-06-06 11:53:30 +0800

    Revert "Add ArgoDB 1.2.0-final"
    This reverts commit ee312e8ab39083b241a11098ade5964eb9c146fb.

--- ARGODB_COMPUTING/argodb-1.2.0-final/templates/executors.ftl
+++
@@ -1,5 +0,0 @@
-<#if service.roles.INCEPTOR_EXECUTOR??>
-    <#list service.roles.INCEPTOR_EXECUTOR as executor>
-    ${executor['hostname']}
-    </#list>
-</#if>
--- ARGODB_STORAGE/argodb-1.2.0-final/templates/shiva/shiva-env.sh
+++
@@ -1,8 +0,0 @@
-<#if service.roles.SHIVA_MASTER?? && service.roles.SHIVA_MASTER?size gt 0>
-  <#assign masters=[]/>
-  <#list service.roles.SHIVA_MASTER as role>
-    <#assign masters += [role.ip + ":" + service["shiva.master.rpc_service.master_service_port"]]>
-  </#list>
-  <#assign master_group = masters?join(",")>
-export MASTER_GROUP=${master_group}
-</#if>
